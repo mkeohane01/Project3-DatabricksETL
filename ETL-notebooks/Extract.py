ad2f00ebb1b5e3d866840b1ba3224430e8ebc62a
mkeohane01 <mkeohane01>
2023-11-14 05:26:03 +0000

    Set up ETL pipeline for stock_data

---
+++ ETL-notebooks/Extract.py
@@ -0,0 +1,35 @@
+# Databricks notebook source
+# Extract the data from stock_data
+
+table_name = "stock_data"
+table_path = "hive_metastore.default.stock_data"
+data = spark.table(table_path)
+
+# Show the first 10 rows of the table
+data.show(10)
+
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC -- Transform using SQL and then Load in to Delta Lake table yearlystocks_delta
+# MAGIC -- Grouping the data by year and averaging the monthly stock prices
+# MAGIC CREATE TABLE
+# MAGIC yearlystocks_delta
+# MAGIC USING DELTA
+# MAGIC SELECT 
+# MAGIC   year(date) AS year, 
+# MAGIC   ROUND(AVG(real),2) AS avg_real, 
+# MAGIC   ROUND(AVG(nominal),2) AS avg_nominal
+# MAGIC FROM 
+# MAGIC   stock_data 
+# MAGIC GROUP BY 
+# MAGIC   year(date)
+# MAGIC ORDER BY
+# MAGIC   year
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC use catalog `hive_metastore`; select * from `default`.`yearlystocks_delta` limit 100;
---
+++ data/stock_data_info.md
@@ -0,0 +1,8 @@
+"MacroTrends Data Download"
+"Dow Jones - DJIA - 100 Year Historical Chart"
+"Note: Real datasets are adjusted for inflation using the headline Consumer Price Index (CPI) with the current month as the base."
+
+"Disclaimer and Terms of Use: Historical data is provided 'as is' and solely for informational purposes, not for trading purposes or advice."
+"MacroTrends LLC expressly disclaims the accuracy, adequacy, or completeness of any data and shall not be liable for any errors, omissions or other defects in, "
+"delays or interruptions in such data, or for any actions taken in reliance thereon.  Neither MacroTrends LLC nor any of our information providers will be liable"
+"for any damages relating to your use of the data provided."
