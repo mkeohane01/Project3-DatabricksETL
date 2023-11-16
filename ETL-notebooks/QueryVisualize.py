6e0d7d87eec11910c5fe82cc39e1608ce3a48b2d
mkeohane01 <mkeohane01>
2023-11-16 00:36:41 +0000

    Updated Visualize to create Matplotlib figures

--- ETL-notebooks/QueryVisualize.py
+++ ETL-notebooks/QueryVisualize.py
@@ -1,7 +1,7 @@
 # Databricks notebook source
 from pyspark.sql import SparkSession
 import matplotlib.pyplot as plt
-from pyspark.sql.functions import col
+import pandas as pd
 
 # initialize spark session
 spark = SparkSession.builder \
@@ -24,18 +24,40 @@ ratio_stock_df = spark.read.table(f"{db}.{ratio_table}")
 
 # COMMAND ----------
 
-# Visualize the data
+# Convert to pandas for plotting
+monthly_stock_pd = monthly_stock_df.toPandas()
+yearly_stock_pd = yearly_stock_df.toPandas()
+ratio_stock_pd = ratio_stock_df.toPandas()
 
-# plot monthly stocks
-display(monthly_stock_df.select('date', 'real').orderBy('date'), mode='matplotlib', 
-        title='Monthly DJIA Stock Prices', xlabel='Date', ylabel='Evaluation')
+fig1, ax1 = plt.subplots()
+ax1.plot(yearly_stock_pd['year'], yearly_stock_pd['avg_real'])
 
-# plot yearly stocks
-display(yearly_stock_df.select('year', 'avg_real').orderBy('year'), mode='matplotlib', 
-        title='Yearly DJIA Stock Prices', xlabel='Year', ylabel='Evaluation')
+ax1.set(xlabel='Year', ylabel='Stock Price',
+       title='Yearly Dow Jones Stock Price')
+ax1.grid()
+
+fig2, ax2 = plt.subplots()
+ax2.plot(ratio_stock_pd['date'], ratio_stock_pd['real_to_nominal_ratio'])
+
+ax2.set(xlabel='Date', ylabel='Real:Nominal Stock Price Ratio',
+       title='Dow Jones Stock- Real:Nominal Ratio')
+ax2.grid()
+
+fig1.savefig('../figs/yearlystockprice.png')
+fig2.savefig('../figs/stockratio.png')
 
 # COMMAND ----------
 
-# plot the ratio
-display(ratio_stock_df.select('date', 'real_to_nominal_ratio').orderBy('date'), mode='matplotlib', 
-        title='Ratio of Real:Nominal', xlabel='Date', ylabel='Ratio')
+# Visualize the data
+
+# # plot monthly stocks
+# display(monthly_stock_df.select('date', 'real').orderBy('date'), mode='matplotlib', 
+#         title='Monthly DJIA Stock Prices', xlabel='Date', ylabel='Evaluation')
+
+# # plot yearly stocks
+# display(yearly_stock_df.select('year', 'avg_real').orderBy('year'), mode='matplotlib', 
+#         title='Yearly DJIA Stock Prices', xlabel='Year', ylabel='Evaluation')
+
+# # plot the ratio
+# display(ratio_stock_df.select('date', 'real_to_nominal_ratio').orderBy('date'), mode='matplotlib', 
+#         title='Ratio of Real:Nominal', xlabel='Date', ylabel='Ratio')
